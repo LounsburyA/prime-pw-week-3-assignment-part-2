5f84df595c038b0e6f8cdf2704fae94ec7c5af5e
Andrew Lounsbury <lounsburya@gmail.com>
2022-02-02 16:51:23 -0600

    improved for loop count by 2

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -16,7 +16,7 @@ for (let i=0; i<=5; i++) {
   console.log(i);
 }
 console.log('count from 0 to 5');
-// need to change the initiliaztion and condition
+// need to change the initiliaztion "0" and condition "5"
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
@@ -24,17 +24,22 @@ console.log('count from 3 to 5');
 for (let i=3; i<=5; i++) {
   console.log(i);
 }
-// need to change the initiliaztion and the condition
+// need to change the initiliaztion "3" and the condition "5"
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
-for (let i=1; i<=10; i++) {
+/*for (let i=1; i<=10; i++) {
   if(i % 2 == 0){
     console.log(i);
   }
 }
-//need to change initiliaztion, condition, and add if statement that divides by 2
+tried this, it works, but too complicated/not what i wanted
+*/
+for (let i=2; i<=10; i+=2) {
+  console.log(i);
+}
+//need to change initiliaztion "2", condition "10", and final expression "+=2"
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
@@ -72,3 +77,8 @@ while (i<5) {
 }
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+let j = 10;
+while (j >=5) {
+console.log(j);
+  j--;
+}
